655ae8ae001ea1558161af48306d6f40af3f4155
VicMontanez <v_montanez@hotmail.com>
2019-12-03 00:31:30 -0500

    updated posts route

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv/config");
 
+//Import Routes
+const postsRoute = require("./routes/posts");
+
+app.use("/posts", postsRoute);
+
 //Routes
 app.get("/", (req, res) => {
   res.send("We are on home");
-});
-
-app.get("/posts", (req, res) => {
-  res.send("We are on post");
 });
 
 //Connect To DB
